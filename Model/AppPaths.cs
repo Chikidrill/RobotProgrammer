53aa99783eb4edb2f0237aa09e5112a1aaa5e908
Kirill Dvornikov <dvornikovkd@yandex.ru>
2026-04-30 17:23:29 +0700

    release ver.1.0.0

---
+++ Model/AppPaths.cs
@@ -0,0 +1,53 @@
+﻿
+public static class AppPaths
+{
+#if DEBUG
+    public const string AppName = "RobotProgrammer.Dev";
+#else
+    public const string AppName = "RobotProgrammer";
+#endif
+
+
+    public static string RoamingAppData =>
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            AppName);
+
+    public static string LocalAppData =>
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            AppName);
+
+    public static string Logs =>
+        Path.Combine(LocalAppData, "logs");
+
+    public static string Temp =>
+        Path.Combine(LocalAppData, "temp");
+
+    public static string Backups =>
+        Path.Combine(LocalAppData, "backups");
+
+    public static string SettingsFile =>
+        Path.Combine(RoamingAppData, "settings.json");
+
+    public static string RecentProjectsFile =>
+        Path.Combine(RoamingAppData, "recent-projects.json");
+
+    public static string DefaultProjectsFolder =>
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+            AppName,
+            "Projects");
+
+    public static void EnsureCreated()
+    {
+        Directory.CreateDirectory(RoamingAppData);
+        Directory.CreateDirectory(LocalAppData);
+        Directory.CreateDirectory(Logs);
+        Directory.CreateDirectory(Temp);
+        Directory.CreateDirectory(Backups);
+        Directory.CreateDirectory(DefaultProjectsFolder);
+    }
+
+
+}
